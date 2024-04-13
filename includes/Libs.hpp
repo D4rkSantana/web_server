f9b88676be493458a9e00db873334a0f2dda165e
D4krSantana <emersonss13@gmail.com>
2024-04-13 11:46:22 -0300

    começando o processamento do cliente

--- includes/Libs.hpp
+++ includes/Libs.hpp
@@ -6,7 +6,7 @@
 /*   By: esilva-s <esilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 16:13:52 by lucasmar          #+#    #+#             */
-/*   Updated: 2024/04/13 00:12:30 by esilva-s         ###   ########.fr       */
+/*   Updated: 2024/04/13 11:24:53 by esilva-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,3 +58,12 @@
 #define COLOR_BLUE "\x1b[34m"
 #define COLOR_GREEN "\x1b[32m"
 #define COLOR_RESET "\x1b[0m"
+
+struct responseData {
+    int         status;
+    std::string content;
+    std::string statusCode;
+    std::string contentType;
+    int         contentLength;
+    std::string location;
+};
